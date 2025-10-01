555e701f834e703d19af0a4c1ffc44b743aa0abe
Kenton Knelle <kken51915@gmail.com>
2025-10-01 09:37:11 -0400

    Exercise 1 Update

--- FactoryPattern/Car.cs
+++ FactoryPattern/Car.cs
@@ -4,9 +4,7 @@ public class Car : IVehicle
 {
     public void Drive()
     {
-        Thread.Sleep(1500);
-        Console.WriteLine("Oh, so you must be driving a Car or a Truck, which one is it?");
-        var CarType =  Console.ReadLine();
-        Console.WriteLine($"Nice, I've always wanted a {CarType}!");
+        Console.WriteLine("Judging by your Input, you are riding a Car or a Truck.");
+
     }
 }
